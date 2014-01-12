c46084027c6b587e0902ccc09f78e6fa4d4d9722
sathishzam <sathishzam@gmail.com>
2014-01-11 19:21:16 -0600

    Update process_16S_illumina.py

--- process_16S_illumina.py
+++ process_16S_illumina.py
@@ -19,6 +19,6 @@ if not os.path.exists("/home/comp/jglab/"+username+"/tmp"):
    print ("No tmp folder detected under home directory, so created directory for future use /home/comp/jglab/"+username+"/tmp\n")
 
 # Ungzip reads 1 through 3 into tmp folder
-#os.system("for f in "+readsfile+"*.gz; do   STEM=$(basename \"${f}\" .gz);   gunzip -c \"${f}\" > /home/comp/jglab/sathish/tmp/\"${STEM}\"; done")
+os.system("for f in "+readsfile+"*.gz; do   STEM=$(basename \"${f}\" .gz);   gunzip -c \"${f}\" > /home/comp/jglab/sathish/tmp/\"${STEM}\"; done")
 
 print ("Fastq files are unzipped, now beginning to trim and overlap R1 and R3") 
